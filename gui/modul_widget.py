f46dbf671b6fb4db2c0b200dcb8c02a27627b7f4
Uskrai <30656271+Uskrai@users.noreply.github.com>
2021-01-05 11:06:31 +0800

    refactor

--- gui/modul_widget.py
+++ gui/modul_widget.py
@@ -10,27 +10,24 @@ class ModulWidget(  QGroupBox ):
         self._button_layout = QHBoxLayout()
         self.layout.addLayout( self._button_layout )
 
-        self._add_button = QPushButton( "Add", self )
-        self._clear_value_button = QPushButton( "Clear Value", self )
-        self._remove_button = QPushButton( "Remove", self )
-        self._clear_modul_button = QPushButton( "Clear Table", self )
-
-        self._button_layout.addWidget( self._add_button )
-        self._button_layout.addWidget( self._clear_value_button )
-        self._button_layout.addWidget( self._remove_button )
-        self._button_layout.addWidget( self._clear_modul_button )
-
         self.layout.setAlignment( Qt.AlignTop )
 
         self._modul_group = ModulGroup( self )
         self.layout.addWidget( self._modul_group )
 
-        self._add_button.clicked.connect( self._modul_group.add_modul )
-        self._remove_button.clicked.connect( self._modul_group.pop_modul )
-        self._clear_modul_button.clicked.connect( self._modul_group.clear_modul )
-        self._clear_value_button.clicked.connect( self._modul_group.clear_value )
+        self._button = {}
+
+        self.add_button( "Add", self._modul_group.add_modul )
+        self.add_button( "Clear Value", self._modul_group.clear_value )
+        self.add_button( "Remove", self._modul_group.pop_modul )
+        self.add_button( "Clear Table", self._modul_group.clear_modul )
 
         self._modul = None
+
+    def add_button( self, name, connect_func ):
+        self._button[name] = QPushButton( name, self )
+        self._button_layout.addWidget( self._button[name] )
+        self._button[name].clicked.connect( connect_func )
 
     def set_modul( self, modul ):
         self._modul = modul
