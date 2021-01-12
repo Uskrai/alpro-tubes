a6319aca5fbda41a7c402850ecf75f9bd9733dbf
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-12 13:26:51 +0700

    menara air yang baru lagi tur

--- fisdas/menara_air.py
+++ fisdas/menara_air.py
@@ -1,6 +1,7 @@
 from base.interfaces import InterfacesBase
 from base.modul import Modul
 import math
+
 class MenaraAir(Modul):
     name = "Menara Air"
     def init_formula(self, interfaces: InterfacesBase):
@@ -8,21 +9,25 @@ class MenaraAir(Modul):
                              brief="Tinggi air diatas lubang",
                              deskripsi="Tinggi air didalam tangki terhitung dari atas lubang tangki",
                              posfix="m")
+        interfaces.get_float("g",
+                             brief="Percepatan gravitasi bumi",
+                             deskripsi="Percepatan gravitasi suatu objek yang berada pada \n\
+                             permukaan laut dikatakan ekuivalen dengan 1 g, yang didefinisikan \n\
+                             memiliki nilai 9,80665 m/s²",
+                             posfix="m/s²")
         interfaces.get_float("h2",
                              brief="Tinggi menara air",
                              deskripsi="Tinggi menara air yang terhitung dari tanah hingga bagian bawah tangki",
                              posfix="m")
-        interfaces.add_func("v", self.kecepatan)
-        interfaces.add_func("x", self.jarak)
+        interfaces.add_func("v", self.menentukan_kecepatan, posfix="m/s")
+        interfaces.add_func("x", self.jarak, posfix="m/s")
 
-    def kecepatan(self, value: dict):
+    def menentukan_kecepatan( self, value : dict):
         h1 = value["h1"]
-        g  = 9.8
-
-        return str(math.sqrt(2*g*h1))
+        g = value["g"]
+        return math.sqrt(2*g*h1)
 
     def jarak(self, value: dict):
         h1 = value["h1"]
         h2 = value["h2"]
-
-        return str(2*math.sqrt(h1*h2))
+        return 2*math.sqrt(h1*h2)
