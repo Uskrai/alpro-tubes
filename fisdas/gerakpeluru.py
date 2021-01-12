ae864040ddab88ac1a6a491bab6c2bc8e0a9eed3
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-12 09:17:00 +0700

    gerak peluru baru

--- fisdas/gerakpeluru.py
+++ fisdas/gerakpeluru.py
@@ -1,39 +1,35 @@
 from base.interfaces import InterfacesBase
 from base.modul import Modul
 import math
-
-
 def kuadrat(val):
     return val ** 2
-
-
-
-
-
-# kalo mau buat kelas(Modul) baru
-# ini filenya dicopy terus ganti nama
-# "Vektor"nya diganti
-# "Moduil"nya jangan
-# kalo udah jadi, masukkin ke file modulfactory.py
 class GerakPeluru(Modul):
-    # ini nama praktikumnya
     name = "Gerak Peluru"
-
-    # ini tempat ngambil nilai
     def init_formula(self, interfaces: InterfacesBase):
-        # kalo mau minta bilangan bulat pake get_int
-        # kalo minta bilangan desimal pake get_float
-        # parameter pertama itu buat namain variablenya pas nanti dioper
-        # kalo make parameter yang sama, nanti ke overwrite jadi hati hati
-        interfaces.get_float("V0")
-        interfaces.get_float("t")
-        interfaces.get_int("teta")
-        interfaces.get_float("Gravitasi")
-
-        # ini buat nambahin fungsi buat ngitung rumusnya,
-        # parameter pertama itu hasil dari apa,
-        # parameter kedua itu fungsinya
-        # ini bisa dipanggil berkali-kali
+        interfaces.get_float("V0",
+                             brief="kecepatan ketika peluru ditembakkan",
+                             deskripsi="Kecepatan awal benda (Vo) adalah \n\
+                             kecepatan mula-mula yang dimiliki oleh benda saat \n\
+                             pertama kali benda bergerak.",
+                             posfix="m/s")
+        interfaces.get_float("t",
+                             brief="waktu",
+                             deskripsi="waktu tempuh adalah waktu total yang dibutuhkan \n\
+                             dalam perjalanan, sudah termasuk berhenti dan tundaan, dari satu \n\
+                             tempat ke tempat lain yang melalui rute tertentu. Disini waktu yang dimaksud\n\
+                             adalah waktu pada saat kecepatan akan ditentukan. ",
+                             posfix= "s")
+        interfaces.get_int("teta",
+                           brief="sudut elevasi ketika peluru ditembakkan",
+                           deskripsi="Sudut elevasi adalah sudut yang dibentuk oleh arah \n\
+                                     horizontal dengan arah pandangan mata pengamat ke arah atas.",
+                           posfix="o")
+        interfaces.get_float("g",
+                             brief="Percepatan gravitasi bumi",
+                             deskripsi="Percepatan gravitasi suatu objek yang berada pada \n\
+                             permukaan laut dikatakan ekuivalen dengan 1 g, yang didefinisikan \n\
+                             memiliki nilai 9,80665 m/s²",
+                             posfix="m/s²")
         interfaces.add_func("Vx", self.Vx)
         interfaces.add_func("Vy", self.Vy)
         interfaces.add_func("Vx^2", self.Vx2)
@@ -42,16 +38,6 @@ class GerakPeluru(Modul):
         interfaces.add_func("X", self.x)
         interfaces.add_func("Y", self.y)
 
-    # ini fungsi buat ngitung rumusnya
-    # parameter kedua itu buat nilai yang diambil dari method
-    # get_value diatas
-    # isi key( [key] ) itu sama kayak yang parameter diatas
-    # kalo diatas interfaces.get_float("a")
-    # ngambilnya value["a"]
-
-    # kalo mau ngambil nama yang dimasukkin
-    # di add_func parameter pertama
-    # bisa ngambil lewat value["_name"]
     def Vx(self, value: dict):
         V0 = value["V0"]
         teta = value["teta"]
