a398780b500142159e788bd38f583dfe6a3a0e16
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-12 08:30:12 +0700

    glb baru

--- fisdas/glb.py
+++ fisdas/glb.py
@@ -3,9 +3,20 @@ from base.modul import Modul
 class GLB( Modul ):
     name = "GLB"
     def init_formula( self, interfaces : InterfacesBase ):
-        interfaces.get_float("v", prefix="m/s" )
-        interfaces.get_float("t", prefix="s")
+        interfaces.get_float("v",
+                             brief="Kecepatan",
+                             deskripsi="kecepatan merupakan cepat lambatnya perubahan posisi \n\
+                             (perpindahan) suatu benda terhadap waktu tempuh, dan merupakan \n\
+                             besaran vektor (memiliki nilai dan arah).",
+                             posfix="m/s" )
+        interfaces.get_float("t",
+                             brief="waktu tempuh",
+                             deskripsi="waktu tempuh adalah waktu total yang dibutuhkan dalam\n\
+                              perjalanan, sudah termasuk berhenti dan tundaan, dari satu tempat \n\
+                              ke tempat lain yang melalui rute tertentu.",
+                             posfix="s")
         interfaces.add_func("s", self.hasil_glb, prefix="m" )
+
     def hasil_glb( self, value : dict ):
         v = value["v"]
         t = value["t"]
