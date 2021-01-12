285bbd8746bb7700e43e98aaa92a13db7187723e
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-12 21:54:59 +0700

    glbb baru

--- fisdas/glbb.py
+++ fisdas/glbb.py
@@ -6,18 +6,18 @@ class GLBB( Modul ):
         interfaces.get_float("Vo",
                              brief="Kecepatan Awal",
                              deskripsi="Kecepatan awal benda (Vo) adalah kecepatan mula-mula yang\n\
-                              dimiliki oleh benda saat pertama kali benda bergerak. ",
+                             dimiliki oleh benda saat pertama kali benda bergerak. ",
                              posfix="m/s")
         interfaces.get_float("a",
                              brief="percepatan",
                              deskripsi="percepatan atau akselerasi adalah perubahan kecepatan dalam\n\
-                                        satuan waktu tertentu. ",
+                             satuan waktu tertentu. ",
                              posfix="m/s<sup>2</sup>")
         interfaces.get_float("t",
                              brief="waktu tempuh",
                              deskripsi="waktu tempuh adalah waktu total yang dibutuhkan dalam\n\
-                                      perjalanan, sudah termasuk berhenti dan tundaan, dari satu tempat \n\
-                                      ke tempat lain yang melalui rute tertentu.",
+                             perjalanan, sudah termasuk berhenti dan tundaan, dari satu tempat \n\
+                             ke tempat lain yang melalui rute tertentu.",
                              posfix="s")
         interfaces.add_func("s", self.hasil_glbb )
 
