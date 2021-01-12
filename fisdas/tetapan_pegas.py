79d0e6444025105c19ab1206d3db951b5a12ce38
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-12 10:45:04 +0700

    tetapan pegas baru

--- fisdas/tetapan_pegas.py
+++ fisdas/tetapan_pegas.py
@@ -9,9 +9,22 @@ class TetapanPegas(Modul):
     name = "Tetapan Pegas"
 
     def init_formula(self, interfaces: InterfacesBase):
-        interfaces.get_float("m")
-        interfaces.get_float("deltay")
-        interfaces.get_float("T")
+        interfaces.get_float("m",
+                             brief="massa",
+                             deskripsi="Massa adalah banyaknya materi yang terkandung \n\
+                             dalam suatu benda. Massa sifatnya konstan atau tidak berubah. ",
+                             posfix="kg")
+        interfaces.get_float("delta y",
+                             brief="perubahan panjang pegas",
+                             deskripsi="perubahan panjang pegas ketika diberikan beban pada \n\
+                                       pegas tersebut",
+                             posfix="m")
+        interfaces.get_float("T",
+                             brief="Periode getaran",
+                             dekripsi="Periode merupakan waktu yang diperlukan suatu benda \n\
+                             untuk melakukan satu getaran/putaran penuh.  Periode getaran dapat \n\
+                             dihitung dari hubungan waktu yang tercatat dibagi dengan jumlah getaran",
+                             posfix="s")
 
         interfaces.add_func("ks", self.konstanta_statis)
         interfaces.add_func("kd", self.konstanta_dinamis)
