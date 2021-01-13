ee8aea3ef4462b8dc57537111083d5805d05951c
Uskrai <30656271+Uskrai@users.noreply.github.com>
2021-01-13 20:25:58 +0800

    removing CLI

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import sys
-from enum import Enum
-from cli.controller import ControllerCLI
 from gui.controller import ControllerGUI
 import modulfactory
 
 
-class Mode(Enum):
-    GUI = 1
-    CLI = 2
-
-def main( mode : Mode ):
-    if mode == Mode.CLI:
-        controller = ControllerCLI()
-    elif mode == Mode.GUI:
-        controller = ControllerGUI()
+def main():
+    controller = ControllerGUI()
     controller.start( modulfactory.get_all_modul() )
 
     exit()
 
-if "--cli" in sys.argv:
-    main( Mode.CLI )
-else:
-    main( Mode.GUI )
+main()
