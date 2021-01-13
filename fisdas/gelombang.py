b31bae55d6fa64825b9306d230159d1082e42bc7
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-13 13:08:40 +0700

    gelombang baru lagi

--- fisdas/gelombang.py
+++ fisdas/gelombang.py
@@ -6,12 +6,12 @@ class Gelombang( Modul ):
         interfaces.get_float("s",
                              brief="Panjang Gelombang Seluruhnya",
                              deskripsi="Panjang gelombang seluruhnya dalam\n\
-                             1 frekuensi dan 1 amplitudo\n",
+1 frekuensi dan 1 amplitudo\n",
                              posfix="m")
         interfaces.get_float("n",
                              brief="jumlah gelombang yang terukur",
                              deskripsi="Jumlah gelombang yang dapat diukur dalam\n\
-                             1 frekuensi dan 1 amplitudo\n")
+1 frekuensi dan 1 amplitudo\n")
         interfaces.get_float("lamda",
                              brief="panjang gelombang",
                              deskripsi="panjang gelombang berdasarkan hasil perhitungan s/n",
@@ -19,7 +19,7 @@ class Gelombang( Modul ):
         interfaces.get_float("f",
                              brief="frekuensi",
                              deskripsi="Frekuensi adalah ukuran jumlah terjadinya sebuah\n\
-                             peristiwa dalam satuan waktu.\n",
+peristiwa dalam satuan waktu.\n",
                              posfix="Hz")
         interfaces.add_func("s / n", self.menentukan_panjang_gelombang, posfix="m")
         interfaces.add_func("lamda * f", self.menentukan_cepat_rambat_gelombang, posfix="m/s")
