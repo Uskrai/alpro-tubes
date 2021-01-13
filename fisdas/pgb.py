a6c82bac6c6661a0917d5ec5eeaa68a629c51f7b
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-13 12:37:44 +0700

    pgb yang baru

--- fisdas/pgb.py
+++ fisdas/pgb.py
@@ -9,33 +9,25 @@ class PGB( Modul ):
         interfaces.get_float("l",
                              brief="panjang tali yang digunakan",
                              deskripsi="Panjang adalah dimensi suatu benda yang \n\
-                             menyatakan jarak antar ujung. Panjang tali diukur menggunakan penggaris",
+menyatakan jarak antar ujung. Panjang tali diukur menggunakan penggaris",
                              posfix="m")
         interfaces.get_float("T",
                              brief="Periode getaran bandul",
                              deskripsi="Periode merupakan waktu yang diperlukan suatu \n\
-                             benda untuk melakukan satu getaran/putaran penuh",
+benda untuk melakukan satu getaran/putaran penuh",
                              posfix="s")
-        interfaces.add_func("Periode", self.periode_getaran )
+        interfaces.get_float("phi",
+                             brief="masukkan bilangan desimal",
+                             deskripsi="phi adalah sebuah konstanata dalam matematika\n\
+yang merupakan perbandingan keliling lingkaran dengan diameter")
         interfaces.add_func("g",self.percepatan_gravitasi_bumi)
-
-    def periode_getaran( self, value : dict ):
-        t = value ["t"]
-        n = value ["n"]
-        
-        if n == 0:
-            return 0
-
-        periode = n/t
-        return periode
 
     def percepatan_gravitasi_bumi(self,value : dict):
         l = value ["l"]
-        n = value ["n"]
-        t = value ["t"]
-
-        if value ["Periode"] == 0:
+        T = value ["T"]
+        phi = value["phi"]
+        if T == 0:
             return 0
 
-        g = (4*(math.pi ** 2)*l) / (value ["Periode"]) ** 2
+        g = 4*(math.pow(phi, 2))*l / math.pow(T, 2)
         return g
