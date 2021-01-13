da89d7a10b81fac9ca5bb877c1f66df088bbd71b
Firda0802 <71951046+firda0802@users.noreply.github.com>
2021-01-13 07:35:57 +0700

    gerak peluru yang baru

--- fisdas/gerakpeluru.py
+++ fisdas/gerakpeluru.py
@@ -9,27 +9,33 @@ class GerakPeluru(Modul):
         interfaces.get_float("V0",
                              brief="kecepatan ketika peluru ditembakkan",
                              deskripsi="Kecepatan awal benda (Vo) adalah \n\
-                             kecepatan mula-mula yang dimiliki oleh benda saat \n\
-                             pertama kali benda bergerak.",
+kecepatan mula-mula yang dimiliki oleh benda saat \n\
+pertama kali benda bergerak.",
                              posfix="m/s")
         interfaces.get_float("t",
                              brief="waktu",
                              deskripsi="waktu tempuh adalah waktu total yang dibutuhkan \n\
-                             dalam perjalanan, sudah termasuk berhenti dan tundaan, dari satu \n\
-                             tempat ke tempat lain yang melalui rute tertentu. Disini waktu yang dimaksud\n\
-                             adalah waktu pada saat kecepatan akan ditentukan. ",
+dalam perjalanan, sudah termasuk berhenti dan tundaan, dari satu \n\
+tempat ke tempat lain yang melalui rute tertentu. Disini waktu yang dimaksud\n\
+adalah waktu pada saat kecepatan akan ditentukan. ",
                              posfix= "s")
         interfaces.get_int("teta",
                            brief="sudut elevasi ketika peluru ditembakkan",
                            deskripsi="Sudut elevasi adalah sudut yang dibentuk oleh arah \n\
-                                     horizontal dengan arah pandangan mata pengamat ke arah atas.",
+horizontal dengan arah pandangan mata pengamat ke arah atas.",
                            posfix="o")
         interfaces.get_float("g",
                              brief="Percepatan gravitasi bumi",
                              deskripsi="Percepatan gravitasi suatu objek yang berada pada \n\
-                             permukaan laut dikatakan ekuivalen dengan 1 g, yang didefinisikan \n\
-                             memiliki nilai 9,80665 m/s²",
+permukaan laut dikatakan ekuivalen dengan 1 g, yang didefinisikan \n\
+memiliki nilai 9,80665 m/s²",
                              posfix="m/s²")
+        interfaces.get_float("Vx",
+                             brief="Mencari kuadrat Vx",
+                             deskripsi="Masukkan kembali nilai Vx")
+        interfaces.get_float("Vy",
+                             brief="Mencari kuadrat Vy",
+                             deskripsi="Masukkan kembali nilai Vy")
         interfaces.add_func("Vx", self.Vx)
         interfaces.add_func("Vy", self.Vy)
         interfaces.add_func("Vx^2", self.Vx2)
@@ -42,44 +48,31 @@ class GerakPeluru(Modul):
         V0 = value["V0"]
         teta = value["teta"]
 
-        Vx = V0 * math.cos(teta)
+        Vx = V0 * math.cos(math.radians(teta))
         return Vx
 
     def Vy(self, value: dict):
         V0 = value["V0"]
         t  = value["t"]
         teta = value["teta"]
-        g = value["Gravitasi"]
+        g = value["g"]
 
-        Vy = V0 * math.sin(teta) - g * t
+        Vy = V0 * math.sin(math.radians(teta)) - g * t
         return Vy
 
     def Vx2(self, value: dict):
-        V0 = value["V0"]
-        teta = value["teta"]
-
-        Vx = V0 * math.cos(teta)
-        Vx2 = kuadrat(Vx)
+        Vx = value["Vx"]
+        Vx2 = math.pow(Vx, 2)
         return Vx2
 
     def Vy2(self, value: dict):
-        V0 = value["V0"]
-        t  = value["t"]
-        teta = value["teta"]
-        g = value["Gravitasi"]
-
-        Vy = V0 * math.sin(teta) - g * t
-        Vy2 = kuadrat(Vy)
+        Vy = value["Vy"]
+        Vy2 = math.pow(Vy, 2)
         return Vy2
 
     def V(self, value: dict):
-        V0 = value["V0"]
-        t  = value["t"]
-        teta = value["teta"]
-        g = value["Gravitasi"]
-
-        Vx = V0 * math.cos(teta)
-        Vy = V0 * math.sin(teta) - g * t
+        Vx = value ["Vx"]
+        Vy = value["Vy"]
         V = kuadrat(Vx + Vy)
         return V
 
@@ -88,16 +81,16 @@ class GerakPeluru(Modul):
         t = value["t"]
         teta = value["teta"]
 
-        x = V0 * math.cos(teta) * t
+        x = V0 * math.cos(math.radians(teta)) * t
         return x
 
     def y(self, value: dict):
         V0 = value["V0"]
         t = value["t"]
         teta = value["teta"]
-        g = value["Gravitasi"]
+        g = value["g"]
 
-        y = (V0 * math.sin(teta)) * t - 0.5 * g * kuadrat(t)
+        y = (V0 * math.sin(math.radians(teta))) * (t - 0.5 * g * math.pow(t, 2))
         return y
 
 
